688f8ed0a62f51235aca781b730eeae0e4fff0b9
RuDevelooper <rudevelooper@gmail.com>
2021-03-14 12:12:55 +0600

    startproject

---
+++ gymstudio/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gymstudio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gymstudio.settings')
+
+application = get_wsgi_application()
