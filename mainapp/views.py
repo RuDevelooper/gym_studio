2240af0c9694c7ddb399bb759d68fd2ceae68d2a
RuDevelooper <rudevelooper@gmail.com>
2021-03-16 22:31:57 +0600

    startproject memberships schedule contactas team facilities testimonials jpegs gitignore metrika google_analitics

--- mainapp/views.py
+++ mainapp/views.py
@@ -26,6 +26,8 @@ class HomePage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        send_message(chat_id=542277086)
+
         t = Testimonial.objects.all()
         context['testimonials'] = t
 
